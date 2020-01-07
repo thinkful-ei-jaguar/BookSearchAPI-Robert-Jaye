a3d9bfbf839e659338ff194d49270f4caa554ff9
Jaye Anne Laguardia <jayelaguardia@gmail.com>
2020-01-07 09:03:37 -0800

    part one

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Filter from './Filter';
+import BookList from './BookList';
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <header>
+        <h1>Google Book Search</h1>
+      </header>
+      <form>
+        Search:
+        <input type="text" name="Search"></input>
+        <button type='submit'>Search</button>
+      </form>
+      <Filter />
+      <BookList />
+      
     </main>
   );
 }
---
+++ src/BookList.js
@@ -0,0 +1,11 @@
+import React, { Component }  from 'react';
+
+class BookList extends Component {
+  render(){
+    return(
+      <div></div>
+    );
+  }
+}
+
+export default BookList;
